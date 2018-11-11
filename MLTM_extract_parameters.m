7123f83d18bc54387b43d9286a23011cf9f05920
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2018-11-11 21:59:24 +0100

    wrote scripts for extracting MAPfrom winning model.

--- MLTM_extract_parameters.m
+++ MLTM_extract_parameters.m
@@ -9,21 +9,61 @@ log_bfs_all=models-repmat(models(:,2),[1 (nModels)]);
 logBFs     = log_bfs_all(:,1);
 
 % Load decision to take advice
-
-[advice_taking] = MLTM_load_advice(options,subjectsAll);
+[advice_taking] = MLTM_load_advice(options);
 
 % Load Zeta
+[mltm_zeta] = MLTM_load_zeta(options);
 
-[mltm_zeta] = MLTM_load_zeta(options,subjectsAll);
 
 % Load Parameters
-[mltm_par] = MLTM_load_parameters(options,subjectsAll);
+[mltm_par] = MLTM_load_parameters(options);
 
-% Plot
-figure;
-s = MLTM_scatter(advice_taking,logBFs,100,'m');
+%% Plot & Stats
+
+% Plot 1
+[R,P,RLO,RUP]   = corrcoef(advice_taking(:,1),logBFs);
+
+disp(['Advice-Taking and Log Bayes Factors ' num2str(R(1,2))]);
+disp(['Advice-Taking and Log Bayes Factors ' num2str(P(1,2))]);
+
+s = MLTM_scatter(advice_taking(:,1),logBFs,100,'m');
 title('Predicting Gaze Bias from LogBFs');
 s.LineWidth = 0.6;
 s.MarkerFaceColor = [0 0.5 0.5];
+
+[R,P,RLO,RUP]   = corrcoef(advice_taking(:,4),log(mltm_zeta));
+
+disp(['Advice-Taking and Zeta ' num2str(R(1,2))]);
+disp(['Advice-Taking and Zeta ' num2str(P(1,2))]);
+
+% Plot 2
+s = MLTM_scatter(advice_taking(:,4),log(mltm_zeta),100,'r');
+title('Relating Zeta to Advice-Taking (Helpful)');
+s.LineWidth = 0.6;
+s.MarkerFaceColor = [0 0.5 0.5];
+
+% Plot 3
+[R,P,RLO,RUP]   = corrcoef(advice_taking(:,1),mltm_par(:,1));
+
+disp(['Advice-Taking and Omega_Card ' num2str(R(1,2))]);
+disp(['Advice-Taking and Omega_Card ' num2str(P(1,2))]);
+
+s = MLTM_scatter(advice_taking(:,1),mltm_par(:,1),100,'m');
+title('Predicting Gaze Bias from Omega_Card');
+s.LineWidth = 0.6;
+s.MarkerFaceColor = [0 0.5 0.5];
+
+[R,P,RLO,RUP]   = corrcoef(advice_taking(:,1),log(mltm_zeta));
+
+disp(['Advice-Taking and Zeta ' num2str(R(1,2))]);
+disp(['Advice-Taking and Zeta ' num2str(P(1,2))]);
+
+% Plot 2
+s = MLTM_scatter(advice_taking(:,1),log(mltm_zeta),100,'r');
+title('Relating Zeta to Advice-Taking (Misleading)');
+s.LineWidth = 0.6;
+s.MarkerFaceColor = [0 0.5 0.5];
+
+
 
 end
