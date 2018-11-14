c5193377c244fda31bad330f0136202d7c100de5
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2018-11-14 13:09:03 +0100

    added new parameters to be used for questionnaire analyses

--- MLTM_load_parameters.m
+++ MLTM_load_parameters.m
@@ -7,9 +7,9 @@ for  iSubject = 1:length(subjectsAll)
     tmp = load(fullfile(options.resultroot,[subjectsAll{iSubject},options.model.perceptualModels{1}, ...
                 options.model.responseModels{1},'.mat']), 'est_int','-mat');
     
-    mltm_par{iSubject,1} = tmp.est_int.p_prc.om_r;
+    mltm_par{iSubject,1} = tmp.est_int.p_prc.ka_r;
     mltm_par{iSubject,2} = tmp.est_int.p_prc.th_r;
-    mltm_par{iSubject,3} = tmp.est_int.p_prc.om_a;
+    mltm_par{iSubject,3} = tmp.est_int.p_prc.ka_a;
     mltm_par{iSubject,4} = tmp.est_int.p_prc.th_a;
 end
 perceptualParameters = cell2mat(mltm_par);
--- MLTM_load_zeta.m
+++ MLTM_load_zeta.m
@@ -8,7 +8,7 @@ for  iSubject = 1:length(subjectsAll)
                 options.model.responseModels{1},'.mat']), 'est_int','-mat');
     
     mltm_zeta{iSubject,1} = tmp.est_int.p_obs.ze1;
-    
+    mltm_zeta{iSubject,2} = tmp.est_int.p_obs.beta;
 end
 zeta = cell2mat(mltm_zeta);
 
